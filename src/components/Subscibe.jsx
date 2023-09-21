6562c878eb00efbe459a79f4e78c3f4ccf62dcb7
Nikhil Gupta <nikhil@onecorp.co.in>
2023-09-21 14:11:12 +0530

    subscribe section implementation

---
+++ src/components/Subscibe.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Subscibe = () => {
+  return (
+    <div className="subscribe-container container">
+      <h3>Subscribe Here</h3>
+      <p>
+        Aliquip do est irure sit aliquip eiusmod deserunt dolor et irure qui ea.
+        Aliquip do est irure sit aliquip eiusmod deserunt dolor et irure qui ea.
+      </p>
+      <button className="theme-btn">Subscribe</button>{" "}
+    </div>
+  );
+};
+
+export default Subscibe;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,6 +6,7 @@ import Calender from '../components/Calender'
 import BlogGrid from '../components/BlogGrid'
 import TargetPoints from '../components/TargetPoints'
 import FAQ from '../components/FAQ'
+import Subscibe from '../components/Subscibe'
 
 const Home = () => {
     return (
@@ -16,6 +17,7 @@ const Home = () => {
             <BlogGrid />
             <TargetPoints />
             <FAQ />
+            <Subscibe />
             <Footer />
         </div>
     )
