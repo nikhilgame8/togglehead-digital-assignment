640a1692078f916498486bca488f03968e9fc9e9
Nikhil Gupta <nikhil@onecorp.co.in>
2023-09-26 00:17:59 +0530

    changes

--- src/components/BlogGrid.jsx
+++ src/components/BlogGrid.jsx
@@ -1,10 +1,24 @@
 import { ArrowRightCircleIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
 
 const BlogGrid = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 6 - 1 ? 0 : prevSlide + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? 6 - 1 : prevSlide - 1));
+  };
+
   return (
     <div className="blog-container hide-scroll-bar container">
-      <div className="grid-container">
+      <div
+        className="grid-container"
+        style={{ transform: `translateX(-${currentSlide * 400}px)` }}
+      >
         {[1, 2, 3, 4, 5, 6, 7, 8].map((item, index) => (
           <div key={index} className="blog-card">
             <h3>Lorem Ipsum</h3>
@@ -18,6 +32,14 @@ const BlogGrid = () => {
             </a>
           </div>
         ))}
+      </div>
+      <div className="grid-carousel-buttons">
+        <button className="prev-slide-button">
+          <ChevronLeftIcon onClick={prevSlide} className="lg-icon" />
+        </button>
+        <button className="next-slide-button">
+          <ChevronRightIcon onClick={nextSlide} className="lg-icon" />
+        </button>
       </div>
     </div>
   );
--- src/components/TargetPoints.jsx
+++ src/components/TargetPoints.jsx
@@ -1,33 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TargetPoints = () => {
+  const [firstPoint, setFirstPoint] = useState(0);
+  const [secondPoint, setSecondPoint] = useState(0);
+  const [thirdPoint, setThirdPoint] = useState(0);
+  const [fourthPoint, setFourthPoint] = useState(0);
+
+  useEffect(() => {
+    const pointsTimer = setInterval(() => {
+      setFirstPoint((prev) => prev !== 125 ? prev + 1 : prev);
+      setSecondPoint((prev) => prev !== 98 ? prev + 1 : prev);
+      setThirdPoint((prev) => prev !== 145 ? prev + 1 : prev);
+      setFourthPoint((prev) => prev !== 132 ? prev + 1 : prev);
+    }, 10);
+    return () => clearInterval(pointsTimer);
+  }, []);
+
   return (
     <div className="target-points-container container">
       <div>
         <div className="text-theme-blue">Lorem Ipsum</div>
-        <h5>123 +</h5>
+        <h5>{firstPoint} +</h5>
         <span>Lorem Ipsum</span>
-        <p>Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim laboris laborum incididunt nostrud eiusmod ad.</p>
+        <p>
+          Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim
+          laboris laborum incididunt nostrud eiusmod ad.
+        </p>
       </div>
       <div>
         <div className="text-theme-blue">Lorem Ipsum</div>
-        <h5>123 +</h5>
+        <h5>{secondPoint} +</h5>
         <span>Lorem Ipsum</span>
-        <p>Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim laboris laborum incididunt nostrud eiusmod ad.</p>
+        <p>
+          Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim
+          laboris laborum incididunt nostrud eiusmod ad.
+        </p>
       </div>
       <div>
         <div className="text-theme-blue">Lorem Ipsum</div>
-        <h5>123 +</h5>
+        <h5>{thirdPoint} +</h5>
         <span>Lorem Ipsum</span>
-        <p>Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim laboris laborum incididunt nostrud eiusmod ad.</p>
+        <p>
+          Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim
+          laboris laborum incididunt nostrud eiusmod ad.
+        </p>
       </div>
       <div>
         <div className="text-theme-blue">Lorem Ipsum</div>
-        <h5>123 +</h5>
+        <h5>{fourthPoint} +</h5>
         <span>Lorem Ipsum</span>
-        <p>Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim laboris laborum incididunt nostrud eiusmod ad.</p>
+        <p>
+          Incididunt nisi deserunt aliquip fugiat commodo adipisicing anim
+          laboris laborum incididunt nostrud eiusmod ad.
+        </p>
       </div>
-      
     </div>
   );
 };
