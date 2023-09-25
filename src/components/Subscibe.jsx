390437c7c0f77703c251cf937cc28c791a7d675a
Nikhil Gupta <nikhil@onecorp.co.in>
2023-09-25 23:32:06 +0530

    changes

--- src/components/Subscibe.jsx
+++ src/components/Subscibe.jsx
@@ -1,16 +1,24 @@
+import { ChevronRightIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
 const Subscibe = () => {
   return (
     <div className="subscribe-container container">
       <div className="subscribe-content">
-        <h3>Subscribe Here</h3>
+        <h3>Subscribe</h3>
         <p>
           Aliquip do est irure sit aliquip eiusmod deserunt dolor et irure qui
           ea. Aliquip do est irure sit aliquip eiusmod deserunt dolor et irure
           qui ea.
         </p>
-        <button className="theme-btn">Subscribe</button>{" "}
+        <div className="subscribe-form">
+          <input
+            type="text"
+            placeholder="Enter your Email Address"
+            className="subscribe-input"
+          />
+          <button className="theme-btn">Subscribe Now <ChevronRightIcon className="sm-icon" /> </button>{" "}
+        </div>
       </div>
     </div>
   );
